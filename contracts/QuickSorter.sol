06b4842f99cadf83cee98c61d75e2e3f6350d3e4
Jakub Wojciechowski <jakub@alice.si>
2017-07-11 17:57:28 +0100

    Remove unused reference implementation of quickSort

--- contracts/QuickSorter.sol
+++ contracts/QuickSorter.sol
@@ -64,23 +64,4 @@ library QuickSorter {
     }
   }
 
-  function quickSort(uint[] storage arr, uint left, uint right) {
-    uint i = left;
-    uint j = right;
-    uint pivot = arr[left + (right - left) / 2];
-    while (i <= j) {
-      while (arr[i] < pivot) i++;
-      while (pivot < arr[j]) j--;
-      if (i <= j) {
-        (arr[i], arr[j]) = (arr[j], arr[i]);
-        i++;
-        j--;
-      }
-    }
-    if (left < j)
-    quickSort(arr, left, j);
-    if (i < right)
-    quickSort(arr, i, right);
-  }
-
 }
